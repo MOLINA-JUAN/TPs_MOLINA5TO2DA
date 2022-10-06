7eb1c0c1d09e127857444ddf30eef5530ccf5798
unknown <juan.molina.epet20@gmail.com>
2022-10-06 08:42:52 -0300

    agrego un segundo region con las funciones de calculo

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,17 @@ let array = [`Juan Molina`,`Lagos Maximo`,`Jeremiaz Lopez`, `Franco Mendez`];
 var an = 6;
 var bn = 8;
 //#endregion
+//#region 
+function sumar (an,bn){
+    return an + bn;
+}
+function restar (an,bn){
+    return an - bn;
+}
+function div (an,bn){
+    return an / bn;
+}
+function mul (an,bn){ //funcion de la multiltiplicacion
+    return an * bn;
+}
+//#endregion
